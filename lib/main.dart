44b54bdc36f936153437f6121637003710350c47
Vishesh Handa <me@vhanda.in>
2018-05-15 16:17:09 +0200

    Add a simple journal list

--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
+
+class JournalList extends StatefulWidget {
+  @override
+  createState() => new JournalListState();
+}
+
+class JournalListState extends State<JournalList> {
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    var createButton = new FloatingActionButton(
+      onPressed: _newPost,
+      child: new Icon(Icons.add),
+    );
+
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Journal'),
+      ),
+      body: _buildSuggestions(),
+      floatingActionButton: createButton,
+    );
+  }
+
+  Widget _buildRow() {
+    var body = "The quick brown fox jumped over the very lazy dog, who then ran"
+        "all around the garden untill he fell down";
+
+    return new ListTile(
+      isThreeLine: true,
+      title: new Text(
+        "May 5, 2018",
+        style: _biggerFont,
+      ),
+      subtitle: new Text("10:24" + "\n" + body),
+      onTap: () {
+        setState(() {});
+      },
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(8.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return new Divider();
+        return _buildRow();
+      },
+    );
+  }
+
+  void _newPost() {
+    print("FOoOO");
+  }
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-        title: 'Startup Name Generator', home: new RandomWords());
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  createState() => new RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = new Set<WordPair>();
-
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  Widget _buildSuggestions() {
-    return new ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return new Divider();
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      },
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-
-    return new ListTile(
-      title: new Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: new Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Startup Name Generator'),
-        actions: <Widget>[
-          new IconButton(
-            icon: new Icon(Icons.list),
-            onPressed: _pushSaved,
-          ),
-        ],
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      new MaterialPageRoute(
-        builder: (context) {
-          final tiles = _saved.map(
-            (pair) {
-              return new ListTile(
-                title: new Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = ListTile
-              .divideTiles(
-                context: context,
-                tiles: tiles,
-              )
-              .toList();
-
-          return new Scaffold(
-            appBar: new AppBar(
-              title: new Text('Saved Suggestions'),
-            ),
-            body: new ListView(children: divided),
-          );
-        },
-      ),
-    );
+    return new MaterialApp(title: 'Journal', home: new JournalList());
   }
 }
