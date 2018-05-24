be53a6c129b3b4292b6cf055e5a8d4943203b319
Vishesh Handa <me@vhanda.in>
2018-05-24 14:42:36 +0200

    Android: Add a test MethodCall
    This is just to test if the Android bridge is working correctly.

--- android/app/src/main/java/com/example/journal/MainActivity.java
+++ android/app/src/main/java/com/example/journal/MainActivity.java
@@ -5,10 +5,60 @@ import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
+// For MethodChannel
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+
+// For battery stuf
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
+
 public class MainActivity extends FlutterActivity {
+  private static final String CHANNEL = "samples.flutter.io/battery";
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+
+    new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
+      new MethodCallHandler() {
+        @Override
+        public void onMethodCall(MethodCall call, Result result) {
+          if (call.method.equals("getBatteryLevel")) {
+            int batteryLevel = getBatteryLevel();
+
+            if (batteryLevel != -1) {
+              result.success(batteryLevel);
+            } else {
+              result.error("UNAVAILABLE", "Battery level not available.", null);
+            }
+          } else {
+            result.notImplemented();
+          }
+        }
+      });
+  }
+
+  private int getBatteryLevel() {
+    int batteryLevel = -1;
+    if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+      BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
+      batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+    } else {
+      Intent intent = new ContextWrapper(getApplicationContext()).
+              registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+      batteryLevel = (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100) /
+              intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+    }
+
+    return batteryLevel;
   }
 }
