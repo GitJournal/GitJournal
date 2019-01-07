967fce65b05df3688cab0552568a289363958c7c
Vishesh Handa <me@vhanda.in>
2019-01-07 16:23:01 +0100

    Minor refactor

--- lib/gitapp.dart
+++ lib/gitapp.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:journal/storage/git.dart';
 
 class GitApp extends StatelessWidget {
   @override
@@ -45,90 +45,4 @@ buildGitButtons() {
       onPressed: gitCommit,
     ),
   ];
-}
-
-Future gitClone() async {
-  const platform = const MethodChannel('gitjournal.io/git');
-
-  print("Going to git clone");
-  try {
-    await platform.invokeMethod('gitClone', {
-      'cloneUrl': "root@bcn.vhanda.in:git/test",
-      'folderName': "journal",
-    });
-    print("Done");
-  } on PlatformException catch (e) {
-    print("gitClone Failed: '${e.message}'.");
-  }
-}
-
-Future generateSSHKeys() async {
-  print("generateSSHKeyss");
-  try {
-    const platform = const MethodChannel('gitjournal.io/git');
-    String publicKey = await platform.invokeMethod('generateSSHKeys', {});
-    print("Public Key " + publicKey);
-  } on PlatformException catch (e) {
-    print("Failed to generateSSHKeys: '${e.message}'.");
-  }
-}
-
-Future gitPull() async {
-  const platform = const MethodChannel('gitjournal.io/git');
-
-  print("Going to git pull");
-  try {
-    await platform.invokeMethod('gitPull', {
-      'folderName': "journal",
-    });
-    print("Done");
-  } on PlatformException catch (e) {
-    print("gitPull Failed: '${e.message}'.");
-  }
-}
-
-Future gitAdd() async {
-  const platform = const MethodChannel('gitjournal.io/git');
-
-  print("Going to git add");
-  try {
-    await platform.invokeMethod('gitAdd', {
-      'folderName': "journal",
-      'filePattern': ".",
-    });
-    print("Done");
-  } on PlatformException catch (e) {
-    print("gitAdd Failed: '${e.message}'.");
-  }
-}
-
-Future gitPush() async {
-  const platform = const MethodChannel('gitjournal.io/git');
-
-  print("Going to git push");
-  try {
-    await platform.invokeMethod('gitPush', {
-      'folderName': "journal",
-    });
-    print("Done");
-  } on PlatformException catch (e) {
-    print("gitPush Failed: '${e.message}'.");
-  }
-}
-
-Future gitCommit() async {
-  const platform = const MethodChannel('gitjournal.io/git');
-
-  print("Going to git commit");
-  try {
-    await platform.invokeMethod('gitCommit', {
-      'folderName': "journal",
-      'authorName': "Vishesh Handa",
-      'authorEmail': "noemail@example.com",
-      'message': "Default message from GitJournal",
-    });
-    print("Done");
-  } on PlatformException catch (e) {
-    print("gitCommit Failed: '${e.message}'.");
-  }
 }
