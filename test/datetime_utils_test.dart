315414ae735e85af6d433c65e8904ff22d00d45f
Vishesh Handa <me@vhanda.in>
2019-01-15 15:35:41 +0100

    Fix datetime to string conversion

--- test/datetime_utils_test.dart
+++ test/datetime_utils_test.dart
@@ -9,5 +9,12 @@ main() {
 
       expect(str, "2011-12-23T10:15:30+00:00");
     });
+
+    test('Test with small date', () {
+      var dateTime = DateTime.utc(2011, 6, 6, 5, 5, 3);
+      var str = toIso8601WithTimezone(dateTime);
+
+      expect(str, "2011-06-06T05:05:03+00:00");
+    });
   });
 }
