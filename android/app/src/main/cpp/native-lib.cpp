cd68df9303b9183d29fc3bd1798fb505913ec13b
Vishesh Handa <me@vhanda.in>
2019-05-13 14:49:49 +0200

    First integration of libgit2 in the Android App
    This will allow us to remove the jgit dependency. JGit is annoying
    because we have to use an old version in order to support older API
    versions. Additionally, there are some subtle differences in their
    implementation.
    
    Finally, for iOS and desktop we will be using libgit2, so we may as well
    use the same stack on Android.

--- android/app/src/main/cpp/native-lib.cpp
+++ android/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,36 @@
 #include <jni.h>
 #include <string>
+#include <stdlib.h>
+
+#include <openssl/err.h>
+#include <openssl/crypto.h>
+#include <libssh2.h>
+#include <git2.h>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_io_gitjournal_gitjournal_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+
+    ERR_load_crypto_strings();
+    const char *openssl_version = OpenSSL_version(0);
+    const char *ssh2_version = libssh2_version(0);
+
+    std::string hello = "Hello from C++: ";
+    hello += openssl_version;
+    hello += " SSH Version: ";
+    hello += ssh2_version;
+
+    //git_libgit2_init();
+    int major;
+    int minor;
+    int patch;
+    git_libgit2_version(&major, &minor, &patch);
+
+    hello += " Git version: ";
+    char n_str[20];
+    sprintf(n_str, "%d.%d.%d", major, minor, patch);
+    hello += n_str;
+
     return env->NewStringUTF(hello.c_str());
 }
