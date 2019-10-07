b4e6786d9a06a6808b8caceeb06e1569c45cdfc8
Vishesh Handa <me@vhanda.in>
2019-10-07 19:24:52 +0200

    Improve the dark theme
    Avoid the green color when in the dark theme. It was clashing quite a
    bit. Lets just go with complete dark shades for now. It's still really
    missing something, though I'm not sure what.

--- lib/themes.dart
+++ lib/themes.dart
@@ -11,9 +11,7 @@ class Themes {
 
   static final dark = ThemeData(
     brightness: Brightness.dark,
-    primaryColor: Color(0xFF66bb6a),
-    primaryColorLight: Color(0xFF98ee99),
-    primaryColorDark: Color(0xFF338a3e),
-    accentColor: Color(0xff6d4c41),
+    primaryColor: Color(0xff212121),
+    accentColor: Color(0xff689f38),
   );
 }
