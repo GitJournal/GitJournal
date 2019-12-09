ad6d47ef0f786925bff6a12e043f584f9de301ce
Vishesh Handa <me@vhanda.in>
2019-12-09 01:56:46 +0100

    Fix dateTime to string with timezone
    We should just use the 'timezone' dart package and not handle this
    ourselves as we're probably fucking it up in other places.
    
    This fixes #42

--- lib/utils/datetime.dart
+++ lib/utils/datetime.dart
@@ -1,21 +1,22 @@
+import 'dart:core';
 import 'package:intl/intl.dart';
 
 String toIso8601(DateTime dt) {
   return DateFormat("yyyy-MM-ddTHH:mm:ss").format(dt);
 }
 
-String toIso8601WithTimezone(DateTime dt) {
+String toIso8601WithTimezone(DateTime dt, [Duration offset]) {
   var result = DateFormat("yyyy-MM-ddTHH:mm:ss").format(dt);
 
-  var offset = dt.timeZoneOffset;
+  offset = offset ?? dt.timeZoneOffset;
   int minutes = (offset.inMinutes % 60);
   int hours = offset.inHours.toInt();
 
-  String minutesStr;
-  if (minutes < 10) {
-    minutesStr = '0' + minutes.toString();
-  } else {
-    minutesStr = minutes.toString();
+  String sign = '+';
+  if (hours < 0) {
+    hours = hours < 0 ? hours * -1 : hours;
+    minutes = minutes < 0 ? minutes * -1 : minutes;
+    sign = '-';
   }
 
   String hourStr;
@@ -25,5 +26,12 @@ String toIso8601WithTimezone(DateTime dt) {
     hourStr = hours.toString();
   }
 
-  return result + '+' + hourStr + ':' + minutesStr;
+  String minutesStr;
+  if (minutes < 10) {
+    minutesStr = '0' + minutes.toString();
+  } else {
+    minutesStr = minutes.toString();
+  }
+
+  return result + sign + hourStr + ':' + minutesStr;
 }
