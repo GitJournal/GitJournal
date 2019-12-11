e5c3bb3fdbb2e7a1fe9ebdbffc731ceb3a4599ce
Vishesh Handa <me@vhanda.in>
2019-12-11 20:43:12 +0100

    Add a 'dev' and 'prod' build flavour
    Now whenever we run 'flutter run' or 'flutter build' we need to pass the
    flavour flag. It seems there is no way to set a default flavour.
    
    This is required as I want to be able to not have to reconfigure my
    local GitJournal app (which I use for note-taking) vs the one I'm
    developing where I might change many different settings for the basis of
    testing.
    
    Specially since I don't use Github/GitLab to store my notes, so the
    setup process is a bit more involved.

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,5 @@ set -eu
 BUILD_NUM=`git rev-list --count HEAD`
 echo "Build Number: $BUILD_NUM"
 
-flutter build appbundle --build-number=$BUILD_NUM --verbose
-flutter build apk --build-number=$BUILD_NUM --verbose
+flutter build appbundle --flavor prod --build-number=$BUILD_NUM --verbose
+flutter build apk --flavor prod --build-number=$BUILD_NUM --verbose
