e61e407739e7f295a43cc4aceb531e03f2c6c4e1
Vishesh Handa <me@vhanda.in>
2020-01-27 23:55:54 +0100

    Theme: Overwrite the cursor color
    This blue cursor color just doesn't go with the rest of the theme.

--- lib/themes.dart
+++ lib/themes.dart
@@ -7,11 +7,13 @@ class Themes {
     primaryColorLight: const Color(0xFF98ee99),
     primaryColorDark: const Color(0xFF338a3e),
     accentColor: const Color(0xff6d4c41),
+    cursorColor: const Color(0xFF66bb6a),
   );
 
   static final dark = ThemeData(
     brightness: Brightness.dark,
     primaryColor: const Color(0xff212121),
     accentColor: const Color(0xff689f38),
+    cursorColor: const Color(0xFF66bb6a),
   );
 }
