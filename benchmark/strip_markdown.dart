17fd25cac9011e95080f4516ca233e330e4f85b1
Vishesh Handa <me@vhanda.in>
2020-02-17 15:49:12 +0100

    Add strip markdown benchmark

---
+++ benchmark/strip_markdown.dart
@@ -0,0 +1,38 @@
+// Import BenchmarkBase class.
+import 'package:benchmark_harness/benchmark_harness.dart';
+import 'package:gitjournal/utils/markdown.dart';
+
+// Create a new benchmark by extending BenchmarkBase
+class StripMarkdownBenchmark extends BenchmarkBase {
+  const StripMarkdownBenchmark() : super('StripMarkdown');
+
+  static void main() {
+    const StripMarkdownBenchmark().report();
+  }
+
+  // The benchmark code.
+  @override
+  void run() {
+    var input = """Itemized lists
+look like:
+
+  * this one
+  * that one
+      """;
+
+    stripMarkdownFormatting(input);
+  }
+
+  // Not measured setup code executed prior to the benchmark runs.
+  @override
+  void setup() {}
+
+  // Not measured teardown code executed after the benchmark runs.
+  @override
+  void teardown() {}
+}
+
+void main() {
+  // Run StripMarkdownBenchmark
+  StripMarkdownBenchmark.main();
+}
