12e16f83e5259210a70165dcdb72d230a2010d03
Vishesh Handa <me@vhanda.in>
2020-02-19 18:06:59 +0100

    KeyEditors: Remove code duplication

--- lib/setup/key_editors.dart
+++ lib/setup/key_editors.dart
@@ -7,31 +7,14 @@ class PublicKeyEditor extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      height: 80.0,
-      child: Container(
-        color: Theme.of(context).buttonColor,
-        child: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              controller: controller,
-              textAlign: TextAlign.left,
-              maxLines: null,
-              style: Theme.of(context).textTheme.body1,
-              autovalidate: true,
-              validator: (String val) {
-                if (!val.startsWith("ssh-")) {
-                  return "Invalid Public Key";
-                }
-                return "";
-              },
-            ),
-          ),
-        ),
-      ),
-    );
+    return KeyEditor(controller, _validator);
+  }
+
+  String _validator(String val) {
+    if (!val.startsWith("ssh-")) {
+      return "Invalid Public Key";
+    }
+    return "";
   }
 }
 
@@ -39,6 +22,28 @@ class PrivateKeyEditor extends StatelessWidget {
   final TextEditingController controller;
 
   PrivateKeyEditor(this.controller);
+
+  @override
+  Widget build(BuildContext context) {
+    return KeyEditor(controller, _validator);
+  }
+
+  String _validator(String val) {
+    if (!val.startsWith("-----BEGIN RSA PRIVATE KEY-----")) {
+      return "Invalid Private Key";
+    }
+    if (!val.startsWith("-----END RSA PRIVATE KEY-----")) {
+      return "Invalid Private Key";
+    }
+    return "";
+  }
+}
+
+class KeyEditor extends StatelessWidget {
+  final TextEditingController controller;
+  final Function validator;
+
+  KeyEditor(this.controller, this.validator);
 
   @override
   Widget build(BuildContext context) {
@@ -56,15 +61,7 @@ class PrivateKeyEditor extends StatelessWidget {
               maxLines: null,
               style: Theme.of(context).textTheme.body1,
               autovalidate: true,
-              validator: (String val) {
-                if (!val.startsWith("-----BEGIN RSA PRIVATE KEY-----")) {
-                  return "Invalid Private Key";
-                }
-                if (!val.startsWith("-----END RSA PRIVATE KEY-----")) {
-                  return "Invalid Private Key";
-                }
-                return "";
-              },
+              validator: validator,
             ),
           ),
         ),
