22c9d1208a49ae0d038c906b7808ab530f24ce9b
Vishesh Handa <me@vhanda.in>
2020-03-21 00:19:57 +0100

    FolderView: Show the correct title

--- lib/screens/folder_view.dart
+++ lib/screens/folder_view.dart
@@ -88,9 +88,15 @@ class _FolderViewState extends State<FolderView> {
       child: Icon(Icons.add),
     );
 
-    var title = widget.notesFolder.parent == null
-        ? "Notes"
-        : widget.notesFolder.pathSpec();
+    // If this is a Virtual folder which doesn't overwrite the FS folder's name
+    // then we should use it's given name as the title
+    String title = widget.notesFolder.name;
+    var fsFolder = widget.notesFolder.fsFolder;
+    if (fsFolder.name == widget.notesFolder.name) {
+      title = widget.notesFolder.parent == null
+          ? "Root Folder"
+          : widget.notesFolder.pathSpec();
+    }
 
     var folderView = Builder(
       builder: (BuildContext context) {
