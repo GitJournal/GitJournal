8c89f8a1b05ceb7ae43cfacedd32a9180a491cd7
Vishesh Handa <me@vhanda.in>
2020-03-26 10:48:08 +0100

    Very basic sentry integration

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart' as foundation;
 
 import 'package:flutter_crashlytics/flutter_crashlytics.dart';
+import 'package:sentry/sentry.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:gitjournal/app.dart';
@@ -32,12 +33,19 @@ void main() async {
     await FlutterCrashlytics().initialize();
   }
 
+  var sentry = SentryClient(
+    dsn: 'https://35f34dbec289435fbe16483faacf49a5@sentry.io/5168082',
+  );
+
   runZoned<Future<void>>(() async {
     await JournalApp.main(pref);
   }, onError: (Object error, StackTrace stackTrace) async {
     print("Uncaught Exception: " + error.toString());
     print(stackTrace);
-    await FlutterCrashlytics()
-        .reportCrash(error, stackTrace, forceCrash: false);
+    FlutterCrashlytics().reportCrash(error, stackTrace, forceCrash: false);
+    sentry.captureException(
+      exception: error,
+      stackTrace: stackTrace,
+    );
   });
 }
