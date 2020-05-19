ce201aa3901f1b02e944d10e6e830374e519ee5d
Vishesh Handa <me@vhanda.in>
2020-05-19 18:36:16 +0200

    Add an experimental Katex rendering Widget
    Related to #125
    
    This still needs to be integrated into the Flutter Markdown renderer.
    But the good news is that this works!
    
    It currently requires network access to download the katex scripts. It
    does NOT send the katex string to any server. The rendering is performed
    in the app.

---
+++ lib/widgets/katex_widget.dart
@@ -0,0 +1,92 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:path/path.dart' as p;
+
+class KatexWidget extends StatefulWidget {
+  final String input;
+
+  KatexWidget(this.input, {Key key}) : super(key: key);
+
+  @override
+  _KatexWidgetState createState() => _KatexWidgetState();
+}
+
+class _KatexWidgetState extends State<KatexWidget> {
+  String imagePath;
+  JavascriptChannel jsChannel;
+
+  final flutterWebViewPlugin = FlutterWebviewPlugin();
+  final selectedUrl = 'https://gitjournal.io/test_katex.html';
+
+  @override
+  void initState() {
+    super.initState();
+
+    jsChannel = JavascriptChannel(
+      name: 'Print',
+      onMessageReceived: (JavascriptMessage message) {
+        print("-----JS CHANNEL ----");
+        print(message.message);
+
+        var uri = UriData.parse(message.message);
+        var tmpFile = p.join(Directory.systemTemp.path, "katex.png");
+        File(tmpFile).writeAsBytesSync(uri.contentAsBytes());
+
+        setState(() {
+          print("State has been set");
+          imagePath = tmpFile;
+        });
+      },
+    );
+
+    flutterWebViewPlugin.onStateChanged.listen((WebViewStateChanged state) {
+      if (!mounted) return;
+
+      if (state.type == WebViewState.finishLoad) {
+        _renderKatex();
+      }
+    });
+
+    flutterWebViewPlugin.close();
+    flutterWebViewPlugin.launch(
+      selectedUrl,
+      hidden: true,
+      javascriptChannels: {jsChannel},
+      withJavascript: true,
+    );
+  }
+
+  @override
+  void dispose() {
+    flutterWebViewPlugin.dispose();
+
+    super.dispose();
+  }
+
+  void _renderKatex() {
+    var katex = widget.input;
+    var js = """katex.render("$katex", document.body, {
+    throwOnError: false
+});
+
+html2canvas(document.body).then(function(canvas) {
+    var img = canvas.toDataURL("image/png");
+    Print.postMessage(img);
+});
+""";
+    flutterWebViewPlugin.evalJavascript(js);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (imagePath == null) {
+      return Container();
+    }
+
+    print("Building Network Image");
+    return Image.file(File(imagePath));
+  }
+}
