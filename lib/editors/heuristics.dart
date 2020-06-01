a5a72117889131fc748044caed061ecfba5418aa
Vishesh Handa <me@vhanda.in>
2020-06-01 21:13:11 +0200

    Bullet Lists: Auto add bullets lists even when modifying in the middle

--- lib/editors/heuristics.dart
+++ lib/editors/heuristics.dart
@@ -12,14 +12,8 @@ EditorHeuristicResult autoAddBulletList(
     return null;
   }
 
-  // Only when getting a new line
+  // Only when adding a new line
   if (curText[cursorPos - 1] != '\n') {
-    return null;
-  }
-
-  // Only add the bullets at the end of the document
-  // FIXME: We should do this anywhere
-  if (curText.length != cursorPos) {
     return null;
   }
 
@@ -35,6 +29,11 @@ EditorHeuristicResult autoAddBulletList(
 
   var indent = match.group(1) ?? "";
   var text = curText.substring(0, cursorPos) + indent + match.group(2) + ' ';
+  if (cursorPos == curText.length) {
+    return EditorHeuristicResult(text, text.length);
+  }
 
-  return EditorHeuristicResult(text, text.length);
+  text += '\n' + curText.substring(cursorPos + 1);
+  var extraChars = indent.length + match.group(2).length + 1;
+  return EditorHeuristicResult(text, cursorPos + extraChars);
 }
--- test/editor_heuristics_test.dart
+++ test/editor_heuristics_test.dart
@@ -11,13 +11,22 @@ void main() {
       expect(result, null);
     });
 
-    test('Adds a bullet item', () {
+    test('Adds a bullet point at the end', () {
       var origText = "Hello\n* One";
       var newText = "Hello\n* One\n";
 
       var result = autoAddBulletList(origText, newText, newText.length);
       expect(result.text, "Hello\n* One\n* ");
       expect(result.cursorPos, result.text.length);
+    });
+
+    test('Adds a bullet point in the middle', () {
+      var origText = "Hello\n* One\n* Three";
+      var newText = "Hello\n* One\n\n* Three";
+
+      var result = autoAddBulletList(origText, newText, 12);
+      expect(result.text, "Hello\n* One\n* \n* Three");
+      expect(result.cursorPos, 14);
     });
   });
 }
