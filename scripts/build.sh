f2d37f2647c37a174d147a209a906488871590cd
Vishesh Handa <me@vhanda.in>
2020-06-01 23:30:31 +0200

    CI: Do not build the APK
    Only build the AAB, since it's all we're uploading. Earlier the apk was
    still useful as I was downloading it for manual testing, but now all
    testing is done by downloading the release from the app store in a
    Closed Testing List.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-set -eu
+set -eux
 
 BUILD_NUM=`git rev-list --count HEAD`
 echo "Build Number: $BUILD_NUM"
 
 flutter build appbundle --flavor prod --build-number=$BUILD_NUM --verbose
-flutter build apk --flavor prod --build-number=$BUILD_NUM --verbose
