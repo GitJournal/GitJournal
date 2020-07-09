915237285efe2150bab57fc2e7bff8460eed8baa
Vishesh Handa <me@vhanda.in>
2020-07-09 10:01:52 +0200

    Force last view to default to Edit

--- lib/settings.dart
+++ lib/settings.dart
@@ -86,6 +86,9 @@ class Settings {
         pref.getString("markdownDefaultView"));
     markdownLastUsedView = SettingsMarkdownDefaultView.fromInternalString(
         pref.getString("markdownLastUsedView"));
+    if (markdownLastUsedView == SettingsMarkdownDefaultView.LastUsed) {
+      markdownLastUsedView = SettingsMarkdownDefaultView.Edit;
+    }
 
     showNoteSummary = pref.getBool("showNoteSummary") ?? showNoteSummary;
     folderViewHeaderType =
