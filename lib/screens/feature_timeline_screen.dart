8861b6a511c5b956b3ec8fde26736be0eb95f777
Vishesh Handa <me@vhanda.in>
2020-08-12 18:06:08 +0200

    Sort imports

--- lib/screens/feature_timeline_screen.dart
+++ lib/screens/feature_timeline_screen.dart
@@ -1,5 +1,7 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+
+import 'package:easy_localization/easy_localization.dart';
+
 import 'package:gitjournal/features.dart';
 
 class FeatureTimelineScreen extends StatelessWidget {
