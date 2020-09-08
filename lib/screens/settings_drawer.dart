59f2098b360f4c479ea5ba31892639e040d88d92
Vishesh Handa <me@vhanda.in>
2020-09-08 23:34:11 +0200

    Start to make sidebar configurable

---
+++ lib/screens/settings_drawer.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'package:easy_localization/easy_localization.dart';
+
+class AppDrawerSettings extends StatefulWidget {
+  @override
+  _AppDrawerSettingsState createState() => _AppDrawerSettingsState();
+}
+
+class _AppDrawerSettingsState extends State<AppDrawerSettings> {
+  @override
+  Widget build(BuildContext context) {
+    var body = Container();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(tr("settings.drawer.title")),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: body,
+    );
+  }
+}
