17b276b9f6b82d9b0f5ab576432cab76e648bc99
Vishesh Handa <me@vhanda.in>
2020-10-05 13:09:39 +0200

    Add a comment on the HomeScreen
    It took me some time to figure out why I had written it this way.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -25,6 +25,9 @@ class _HomeScreenState extends State<HomeScreen> {
     Future.delayed(Duration.zero, _initFolder);
   }
 
+  // This is nto done inside build as we want to avoid rebuilding the
+  // FlattenedNotesFolder as much as possible. It's very expensive, since
+  // it sorts all the notes.
   void _initFolder() async {
     if (!mounted) return;
 
