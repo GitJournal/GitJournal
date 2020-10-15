2c0470a3c14917767a914531e0f813c330da3abb
Vishesh Handa <me@vhanda.in>
2020-10-15 10:07:48 +0200

    Log the gitBaseDir to try and debug what is going on

--- lib/app.dart
+++ lib/app.dart
@@ -61,6 +61,11 @@ class JournalApp extends StatefulWidget {
 
     var dir = await getApplicationDocumentsDirectory();
     appState.gitBaseDirectory = dir.path;
+    Log.i("GitBaseDirectory: ${dir.path}");
+    await for (var fsEntity in dir.list()) {
+      Log.i("    ${fsEntity.path}");
+    }
+    Log.i('-----');
 
     if (settings.localGitRepoConfigured == false) {
       // FIXME: What about exceptions!
