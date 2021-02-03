b301d70991d7b25a7373e3b3886f0d1c30c2e97f
Vishesh Handa <me@vhanda.in>
2021-02-03 17:10:40 +0100

    Add convenient script for running tests

---
+++ scripts/watch_test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -eu
+
+# Runs the test which is marked with solo
+
+FILENAME=$(grep -rn solo test/* | awk -F ':' '{ print $1 }')
+LINE_NUM=$(grep -rn solo test/* | awk -F ':' '{ print $2 }')
+
+FUNC_NAME=""
+while [[ -z $FUNC_NAME ]]; do
+    LINE_NUM=$((LINE_NUM - 1))
+    LINE=$(sed -n ${LINE_NUM}p test/autocompletion/tags_test.dart)
+    FUNC_NAME=$(echo "$LINE" | sed -n -E "s#test\([\"'](.+)[\"'].*#\1#p" | xargs)
+done
+
+echo flutter test --name "$FUNC_NAME" "$FILENAME"
+find . -name '*.dart' | entr flutter test --name "$FUNC_NAME" "$FILENAME"
