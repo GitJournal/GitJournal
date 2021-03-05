e62bb863695f1c3c87d26e41b30d1e4cd5c6d547
Alen Šiljak <code@alensiljak.eu.org>
2021-03-05 10:19:50 +0100

    common build fixes

--- lib/widgets/note_editor_selector.dart
+++ lib/widgets/note_editor_selector.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:easy_localization/easy_localization.dart';
-import 'package:font_awesome_flutter/fa_icon.dart';
+// import 'package:font_awesome_flutter/fa_icon.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'package:gitjournal/core/note.dart';
