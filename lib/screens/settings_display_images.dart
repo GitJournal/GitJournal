b79611cde591dd32d199cbceb49e094108f94050
Roland Fredenhagen <important@van-fredenhagen.de>
2021-03-05 01:07:19 +0100

    feat: :sparkles: SVG Rendering, Image theming and Image caption
    fixes issues #405 #410 #246

---
+++ lib/screens/settings_display_images.dart
@@ -0,0 +1,77 @@
+/*
+Copyright 2020-2021 Roland Fredenhagen <important@van-fredenhagen.de>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import 'package:flutter/material.dart';
+
+import 'package:easy_localization/easy_localization.dart';
+
+import 'package:gitjournal/screens/settings_display_images_caption.dart';
+import 'package:gitjournal/screens/settings_display_images_theming.dart';
+
+class SettingsDisplayImagesScreen extends StatefulWidget {
+  @override
+  SettingsDisplayImagesScreenState createState() =>
+      SettingsDisplayImagesScreenState();
+}
+
+class SettingsDisplayImagesScreenState
+    extends State<SettingsDisplayImagesScreen> {
+  final doNotThemeTagsKey = GlobalKey<FormFieldState<String>>();
+  final doThemeTagsKey = GlobalKey<FormFieldState<String>>();
+
+  @override
+  Widget build(BuildContext context) {
+    final body = ListView(children: <Widget>[
+      ListTile(
+        title: Text(tr("settings.display.images.theming.title")),
+        subtitle: Text(tr("settings.display.images.theming.subtitle")),
+        onTap: () {
+          var route = MaterialPageRoute(
+            builder: (context) => SettingsDisplayImagesThemingScreen(),
+            settings:
+                const RouteSettings(name: '/settings/display_images/theming'),
+          );
+          Navigator.of(context).push(route);
+        },
+      ),
+      ListTile(
+        title: Text(tr("settings.display.images.captions.title")),
+        subtitle: Text(tr("settings.display.images.captions.subtitle")),
+        onTap: () {
+          var route = MaterialPageRoute(
+            builder: (context) => SettingsDisplayImagesCaptionScreen(),
+            settings:
+                const RouteSettings(name: '/settings/display_images/caption'),
+          );
+          Navigator.of(context).push(route);
+        },
+      ),
+    ]);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(tr('settings.display.images.title')),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: body,
+    );
+  }
+}
