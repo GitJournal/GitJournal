1b6c76abb266b0fb809906be1e9363e637a2526c
Vishesh Handa <me@vhanda.in>
2021-03-15 10:28:35 +0100

    Remove unused header

--- lib/widgets/note_editor_selector.dart
+++ lib/widgets/note_editor_selector.dart
@@ -17,7 +17,6 @@ limitations under the License.
 import 'package:flutter/material.dart';
 
 import 'package:easy_localization/easy_localization.dart';
-import 'package:font_awesome_flutter/fa_icon.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'package:gitjournal/core/note.dart';
