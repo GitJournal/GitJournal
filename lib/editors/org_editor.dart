3a5a742c1d831c1abd42cd767a387fbdbcaa4c0d
Alen Šiljak <code@alensiljak.eu.org>
2021-03-23 11:49:37 +0100

    updating the name on the license

--- lib/editors/org_editor.dart
+++ lib/editors/org_editor.dart
@@ -1,5 +1,5 @@
 /*
-Copyright 2020-2021 Vishesh Handa <me@vhanda.in>
+Copyright 2020-2021 Alen Šiljak <gitjournal@alensiljak.eu.org>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
