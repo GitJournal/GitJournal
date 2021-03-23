68388616e528e78cfbb29ccbf75ac54f2773fc82
Vishesh Handa <me@vhanda.in>
2021-03-23 09:28:50 +0100

    Remove isolates_workaround
    No longer required

--- test_driver/main_test.dart
+++ test_driver/main_test.dart
@@ -3,12 +3,9 @@ import 'package:screenshots/screenshots.dart';
 import 'package:test/test.dart';
 import 'package:time/time.dart';
 
-import 'isolates_workaround.dart';
-
 void main() {
   group('Test', () {
     FlutterDriver driver;
-    IsolatesWorkaround workaround;
 
     int screenshotNum = 0;
     final config = Config();
@@ -16,18 +13,8 @@ void main() {
     // Connect to the Flutter driver before running any tests
     setUpAll(() async {
       driver = await FlutterDriver.connect();
-      workaround = IsolatesWorkaround(driver);
-      await workaround.resumeIsolates();
 
       await Future.delayed(15.seconds);
-    });
-
-    // Close the connection to the driver after the tests have completed
-    tearDownAll(() async {
-      if (driver != null) {
-        await driver.close();
-        await workaround.tearDown();
-      }
     });
 
     Future _takeScreenshot() async {
