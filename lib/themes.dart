445f35ab64a9d3364c7a12265fc9fa4364b6da0c
Vishesh Handa <me@vhanda.in>
2021-03-24 15:48:51 +0100

    theme: stop using deprecated members

--- lib/themes.dart
+++ lib/themes.dart
@@ -7,16 +7,20 @@ class Themes {
     primaryColorLight: const Color(0xFF98ee99),
     primaryColorDark: const Color(0xFF338a3e),
     accentColor: const Color(0xff6d4c41),
-    cursorColor: const Color(0xFF338a3e),
-    textSelectionHandleColor: const Color(0xFF66bb6a),
+    textSelectionTheme: const TextSelectionThemeData(
+      cursorColor: Color(0xFF338a3e),
+      selectionHandleColor: Color(0xFF66bb6a),
+    ),
   );
 
   static final dark = ThemeData(
     brightness: Brightness.dark,
     primaryColor: const Color(0xff212121),
     accentColor: const Color(0xff689f38),
-    cursorColor: const Color(0xFF66bb6a),
-    textSelectionHandleColor: const Color(0xFF66bb6a),
     toggleableActiveColor: const Color(0xFF66bb6a),
+    textSelectionTheme: const TextSelectionThemeData(
+      cursorColor: Color(0xFF66bb6a),
+      selectionHandleColor: Color(0xFF66bb6a),
+    ),
   );
 }
