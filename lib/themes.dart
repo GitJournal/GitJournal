88e57bb7051c0690520b5d1c3ad3cdcab2d44c88
Vishesh Handa <me@vhanda.in>
2021-06-06 01:29:30 +0200

    Add 'colorScheme' to the themes
    This way there are no blue colours anymore, and the themes are kind of
    better.

--- lib/themes.dart
+++ lib/themes.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 
 class Themes {
   static final light = ThemeData(
+    colorScheme: ColorScheme.fromSwatch(
+      primarySwatch: Colors.green,
+    ).copyWith(
+      primary: const Color(0xFF66bb6a),
+      secondary: const Color(0xff6d4c41),
+    ),
     brightness: Brightness.light,
     primaryColor: const Color(0xFF66bb6a),
     primaryColorLight: const Color(0xFF98ee99),
@@ -14,6 +20,13 @@ class Themes {
   );
 
   static final dark = ThemeData(
+    colorScheme: ColorScheme.fromSwatch(
+      primarySwatch: Colors.grey,
+      brightness: Brightness.dark,
+    ).copyWith(
+      primary: const Color(0xff212121),
+      secondary: const Color(0xff689f38),
+    ),
     brightness: Brightness.dark,
     primaryColor: const Color(0xff212121),
     accentColor: const Color(0xff689f38),
