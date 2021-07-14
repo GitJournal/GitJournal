6abee38adf443589714d5be28f060ef3ef42a2b5
Vishesh Handa <me@vhanda.in>
2021-07-14 10:30:18 +0200

    Remove unused header

--- lib/folder_views/folder_view.dart
+++ lib/folder_views/folder_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:easy_localization/easy_localization.dart';
-import 'package:git_bindings/git_bindings.dart';
 import 'package:provider/provider.dart';
 
 import 'package:gitjournal/app.dart';
