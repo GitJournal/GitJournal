79b74d0d77a580f1bee88c9a2ba791c600fa9ce4
Vishesh Handa <me@vhanda.in>
2021-07-30 12:56:15 +0200

    Show progress on the cloning page
    Fixes #397

--- lib/setup/cloning.dart
+++ lib/setup/cloning.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:easy_localization/easy_localization.dart';
+import 'package:sprintf/sprintf.dart';
 
 import 'error.dart';
 import 'git_transfer_progress.dart';
@@ -18,10 +19,63 @@ class GitHostCloningPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (errorMessage == null || errorMessage!.isEmpty) {
+    if (errorMessage != null && errorMessage!.isNotEmpty) {
+      return GitHostSetupErrorPage(errorMessage!);
+    }
+
+    var stats = cloneProgress;
+    if (stats.totalObjects == 0) {
       return GitHostSetupLoadingPage(loadingMessage);
     }
 
-    return GitHostSetupErrorPage(errorMessage!);
+    var fetchPercent = (100 * stats.receivedObjects) / stats.totalObjects;
+    var indexPercent = (100 * stats.indexedObjects) / stats.totalObjects;
+    var kbytes = stats.receivedBytes ~/ 1024;
+
+    var networkText = sprintf("network %0.3f%% (%4d kb, %5d/%5d)", [
+      fetchPercent,
+      kbytes,
+      stats.receivedObjects,
+      stats.totalObjects,
+    ]);
+    var indexText = sprintf('index %0.3f%% (%5d/%5d)', [
+      indexPercent,
+      stats.indexedObjects,
+      stats.totalObjects,
+    ]);
+    var text = '$networkText\n$indexText';
+
+    var children = <Widget>[
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          loadingMessage,
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.headline4,
+        ),
+      ),
+      const SizedBox(height: 8.0),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          text,
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.bodyText1,
+        ),
+      ),
+      const SizedBox(height: 8.0),
+      const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: CircularProgressIndicator(
+          value: null,
+        ),
+      ),
+    ];
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: children,
+    );
   }
 }
