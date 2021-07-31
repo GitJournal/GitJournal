a181cd9e2086f3852c4959a9284e2d55b05c4b5d
Vishesh Handa <me@vhanda.in>
2021-07-31 17:18:46 +0200

    Add a markdown TextVisitor class
    This is going to be used to parse the tags from a markdown document
    while ignoring the text inside a code block

---
+++ lib/core/processors/text_visitor.dart
@@ -0,0 +1,32 @@
+import 'package:function_types/function_types.dart';
+import 'package:markdown/markdown.dart' as md;
+
+/// Called for each text node of the markdown document. It is not called
+/// for text inside a code block
+class TextVisitor implements md.NodeVisitor {
+  Func1<String, void> textCallback;
+
+  TextVisitor(this.textCallback);
+
+  @override
+  bool visitElementBefore(md.Element el) {
+    if (el.tag == 'code') {
+      return false;
+    }
+    return true;
+  }
+
+  @override
+  void visitText(md.Text text) {
+    textCallback(text.text);
+  }
+
+  @override
+  void visitElementAfter(md.Element el) {}
+
+  void visit(List<md.Node> nodes) {
+    for (final node in nodes) {
+      node.accept(this);
+    }
+  }
+}
---
+++ test/processors/text_visitor_test.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+
+import 'package:function_types/function_types.dart' as fn;
+import 'package:markdown/markdown.dart' as md;
+import 'package:test/test.dart';
+
+import 'package:gitjournal/core/processors/text_visitor.dart';
+
+void main() {
+  void _visit(String body, fn.Func1<String, void> textCallback) {
+    final doc = md.Document(
+      encodeHtml: false,
+      extensionSet: md.ExtensionSet.gitHubFlavored,
+    );
+
+    var lines = LineSplitter.split(body).toList();
+    var nodes = doc.parseLines(lines);
+
+    TextVisitor(textCallback).visit(nodes);
+  }
+
+  test('Simple Test', () {
+    var body = "Hello *Hi* **How**";
+
+    var result = "";
+    var callback = (String content) {
+      result += content + "-";
+    };
+
+    _visit(body, callback);
+    expect(result, "Hello -Hi- -How-");
+  });
+
+  test('Inline code block', () {
+    var body = "Hello`foo`";
+
+    var result = "";
+    var callback = (String content) {
+      result += content + "-";
+    };
+
+    _visit(body, callback);
+    expect(result, "Hello-");
+  });
+
+  test('Big code block', () {
+    var body = """Hi
+```
+foo
+```
+Done
+""";
+
+    var result = "";
+    var callback = (String content) {
+      result += content + "-";
+    };
+
+    _visit(body, callback);
+    expect(result, "Hi-Done-");
+  });
+}
