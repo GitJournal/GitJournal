62115382d78ac49f062a0a0df273953c90d64b4f
Vishesh Handa <me@vhanda.in>
2021-09-07 18:31:11 +0200

    Linux: Give the correct build number and name

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -4,8 +4,14 @@ set -eux
 
 cd "$(dirname "$0")/.."
 
+BUILD_NUM=$(git rev-list --count HEAD)
+echo "Build Number: $BUILD_NUM"
+
+BUILD_NAME=$(cat pubspec.yaml | grep version | awk '{ print $2 }' | awk -F "+" '{ print $1 }')
+echo "Build Name: $BUILD_NAME"
+
 flutter config --enable-linux-desktop
-flutter build linux
+flutter build linux --release --build-number="$BUILD_NUM" --build-name="$BUILD_NAME"
 
 sed -i "s|_CODE_PATH_|$(pwd)|" AppImageBuilder.yml
 appimage-builder --skip-test
