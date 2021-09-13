e447962fdb36789ff80070ae06b4fa1c6fd74af2
Vishesh Handa <me@vhanda.in>
2021-09-13 17:40:59 +0200

    Bump flutter version everywhere

--- scripts/ios_install_flutter.sh
+++ scripts/ios_install_flutter.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-wget -O flutter.zip https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_2.2.3-stable.zip
+wget -O flutter.zip https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_2.5.0-stable.zip
 unzip flutter.zip
 export PATH="$PATH:$(pwd)/flutter/bin"
 
