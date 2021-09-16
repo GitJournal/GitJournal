b631914ffaee0f2afffb4f4c3d436d522b6c3138
Vishesh Handa <me@vhanda.in>
2021-09-16 10:57:03 +0200

    More license headers

--- test_driver/main.dart
+++ test_driver/main.dart
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2019-2021 Vishesh Handa <me@vhanda.in>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 import 'package:dart_git/git.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
