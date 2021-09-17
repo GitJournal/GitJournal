e12eb634ff76867ff16d05a2f1a48bdd7023f54e
Vishesh Handa <me@vhanda.in>
2021-09-17 11:13:38 +0200

    Add a script to print the current version

---
+++ scripts/version.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+# SPDX-FileCopyrightText: 2019-2021 Vishesh Handa <me@vhanda.in>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+set -eu
+
+VERSION=$(grep version pubspec.yaml | awk '{ print $2 }' | awk '{split($0,a,"+"); print a[1]}')
+
+echo "Build Version: $VERSION"
+echo "Build Number :   $(git rev-list HEAD --count)"
