424743746cdf933a1f88a3f0d6f309c80ce76ac0
Vishesh Handa <me@vhanda.in>
2021-11-07 18:25:32 +0100

    Typo

--- lib/folder_views/folder_view.dart
+++ lib/folder_views/folder_view.dart
@@ -86,7 +86,7 @@ class _FolderViewState extends State<FolderView> {
       folder: await FlattenedFilteredNotesFolder.load(
         widget.notesFolder,
         title: LocaleKeys.widgets_FolderView_pinned,
-        filter: (Note note) async => note.pinned,
+        filter: (Note note) async => !note.pinned,
       ),
       sortingMode: widget.notesFolder.config.sortingMode,
     );
