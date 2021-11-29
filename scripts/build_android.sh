d4aef9468ea83f1e0b3555aa188ba7fa51e95970
Vishesh Handa <me@vhanda.in>
2021-11-29 17:19:49 +0100

    CI: Build 'dev' version apk
    This way I can also publish this on FDroid.

--- scripts/build_android.sh
+++ scripts/build_android.sh
@@ -11,8 +11,11 @@ echo "Build Number: $BUILD_NUM"
 
 flutter build appbundle --flavor prod --build-number="$BUILD_NUM" --dart-define=INSTALL_SOURCE=playstore --verbose
 
+BUILD_ID=$(make version | tail -n 1 | awk '{ print $4 }')
+
 # Also building the apk, as it's useful for non Google Play users
 flutter build apk --flavor prod --build-number="$BUILD_NUM" --dart-define=INSTALL_SOURCE=fdroid --verbose
-
-BUILD_ID=$(make version | tail -n 1 | awk '{ print $4 }')
 cp build/app/outputs/flutter-apk/app-prod-release.apk io.gitjournal.gitjournal_$BUILD_ID.apk
+
+flutter build apk --flavor dev --build-number="$BUILD_NUM" --dart-define=INSTALL_SOURCE=fdroid --verbose
+cp build/app/outputs/flutter-apk/app-dev-release.apk io.gitjournal.gitjournal.dev_$BUILD_ID.apk
