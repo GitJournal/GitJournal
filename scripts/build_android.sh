c965584c850612c9dace3426bd0f18410ad5a20f
Vishesh Handa <me@vhanda.in>
2021-11-29 16:17:47 +0100

    Android: Rename build artifact to follow fdroid naming scheme

--- scripts/build_android.sh
+++ scripts/build_android.sh
@@ -15,4 +15,4 @@ flutter build appbundle --flavor prod --build-number="$BUILD_NUM" --dart-define=
 flutter build apk --flavor prod --build-number="$BUILD_NUM" --dart-define=INSTALL_SOURCE=fdroid --verbose
 
 BUILD_ID=$(make version | tail -n 1 | awk '{ print $4 }')
-cp build/app/outputs/flutter-apk/app-prod-release.apk gitjournal-$BUILD_ID.apk
+cp build/app/outputs/flutter-apk/app-prod-release.apk io.gitjournal.gitjournal_$BUILD_ID.apk
