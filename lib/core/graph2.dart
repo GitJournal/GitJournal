f854797d594b00f581ff12a905fc907c847c54a5
Vishesh Handa <me@vhanda.in>
2021-12-16 19:56:23 +0100

    Try use d3-force-flutter for the graph view
    It's buggy and my own graph drawing algorithm seemed to perform better.

---
+++ lib/core/graph2.dart
@@ -0,0 +1,71 @@
+import 'package:d3_force_flutter/d3_force_flutter.dart' as d3f;
+import 'package:gitjournal/core/folder/notes_folder.dart';
+import 'package:gitjournal/core/folder/notes_folder_fs.dart';
+import 'package:gitjournal/core/views/note_links_view.dart';
+import 'package:gitjournal/utils/link_resolver.dart';
+
+import 'note.dart';
+
+class NoteNode extends d3f.Node {
+  final Note note;
+
+  NoteNode(this.note);
+}
+
+class Graph {
+  final List<NoteNode> nodes;
+  final List<d3f.Edge<NoteNode>> edges;
+
+  Graph(this.nodes, this.edges);
+}
+
+Future<Graph> buildGraph(
+  NotesFolderFS rootFolder,
+  NoteLinksView linksView,
+) async {
+  var nodes = <NoteNode>[];
+  var edges = <d3f.Edge<NoteNode>>[];
+
+  // FIXME: Make this faster?
+  NoteNode _getNode(Note note) {
+    var i = nodes.indexWhere((n) => n.note.filePath == note.filePath);
+    if (i == -1) {
+      var node = NoteNode(note);
+      nodes.add(node);
+      return node;
+    }
+
+    return nodes[i];
+  }
+
+  Future<void> _addNote(Note note) async {
+    var node = _getNode(note);
+
+    var links = await linksView.fetchLinks(note);
+    var linkResolver = LinkResolver(note);
+    for (var l in links) {
+      var noteB = linkResolver.resolveLink(l);
+      if (noteB == null) {
+        // print("not found $l");
+        continue;
+      }
+
+      var edge = d3f.Edge(source: node, target: _getNode(noteB));
+      edges.add(edge);
+    }
+  }
+
+  Future<void> _addFolder(NotesFolder folder) async {
+    for (var note in folder.notes) {
+      await _addNote(note);
+    }
+
+    for (var subFolder in folder.subFolders) {
+      await _addFolder(subFolder);
+    }
+  }
+
+  await _addFolder(rootFolder);
+
+  return Graph(nodes, edges);
+}
--- lib/screens/graph_view.dart
+++ lib/screens/graph_view.dart
@@ -4,56 +4,84 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:gitjournal/generated/locale_keys.g.dart';
+import 'package:gitjournal/widgets/app_drawer.dart';
 
 import 'package:provider/provider.dart';
 
 import 'package:gitjournal/core/folder/notes_folder_fs.dart';
-import 'package:gitjournal/core/graph.dart';
+import 'package:gitjournal/core/graph2.dart';
 import 'package:gitjournal/core/views/note_links_view.dart';
 
-class GraphViewScreen extends StatefulWidget {
+import 'package:d3_force_flutter/d3_force_flutter.dart' as f;
+
+class GraphViewScreen extends StatelessWidget {
   static const routePath = '/graph';
 
   const GraphViewScreen({Key? key}) : super(key: key);
 
   @override
+  Widget build(BuildContext context) {
+    var rootFolder = Provider.of<NotesFolderFS>(context);
+    var linksView = NoteLinksProvider.of(context);
+
+    return _GraphViewScreen(
+      rootFolder: rootFolder,
+      linksView: linksView,
+    );
+  }
+}
+
+class _GraphViewScreen extends StatefulWidget {
+  final NotesFolderFS rootFolder;
+  final NoteLinksView linksView;
+
+  const _GraphViewScreen({
+    required this.rootFolder,
+    required this.linksView,
+    Key? key,
+  }) : super(key: key);
+
+  @override
   _GraphViewScreenState createState() => _GraphViewScreenState();
 }
 
-class _GraphViewScreenState extends State<GraphViewScreen> {
+class _GraphViewScreenState extends State<_GraphViewScreen> {
   Graph? graph;
 
   @override
-  Widget build(BuildContext context) {
-    if (graph == null) {
-      var rootFolder = Provider.of<NotesFolderFS>(context);
-      var linksProvider = NoteLinksProvider.of(context);
+  void initState() {
+    super.initState();
 
-      setState(() {
-        graph = Graph.fromFolder(rootFolder, linksProvider);
-        graph!.addListener(_setState);
-      });
-      return const SizedBox(width: 2500, height: 2500);
-    }
+    () async {
+      graph = await buildGraph(widget.rootFolder, widget.linksView);
+      print("Finished building the graph ...");
 
-    return SafeArea(child: graph != null ? GraphView(graph!) : Container());
+      print('nodes: ${graph!.nodes.length}');
+      print('edges: ${graph!.edges.length}');
+    }();
   }
 
   @override
-  void dispose() {
-    if (graph != null) {
-      graph!.stopLayout();
-      graph!.removeListener(_setState);
-    }
-
-    super.dispose();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: AppDrawer(),
+      appBar: AppBar(
+        title: Text(LocaleKeys.drawer_graph.tr()),
+      ),
+      body: graph != null ? GraphView(graph!) : Container(),
+    );
   }
+}
 
-  void _setState() {
-    if (!mounted) return;
+class _GraphListenable extends ChangeNotifier {
+  _GraphListenable();
 
-    setState(() {});
+  void notify() {
+    notifyListeners();
   }
 }
 
@@ -66,77 +94,126 @@ class GraphView extends StatefulWidget {
   _GraphViewState createState() => _GraphViewState();
 }
 
-class _GraphViewState extends State<GraphView> {
+class _GraphViewState extends State<GraphView>
+    with SingleTickerProviderStateMixin {
   final nodeSize = 50.0;
   late TransformationController transformationController;
+
+  late final f.ForceSimulation simulation;
+  late final Ticker _ticker;
+  late final Size _size;
+  late final _GraphListenable _graphListenable;
 
   @override
   void initState() {
     super.initState();
 
-    widget.graph.addListener(() {
-      if (!mounted) return;
-      setState(() {});
-    });
+    _size = const Size(2500, 2500);
+    var graph = widget.graph;
+
+    const _m = 1.0;
+
+    simulation = f.ForceSimulation(
+      phyllotaxisX: _size.width / 2,
+      phyllotaxisY: _size.height / 2,
+      phyllotaxisRadius: 20,
+    )
+      ..nodes = graph.nodes
+      ..setForce('collide', f.Collide(radius: 10 * _m))
+      // ..setForce('radial', f.Radial(radius: 400 * _m))
+      ..setForce('manyBody', f.ManyBody(strength: -40 * 3.0 * _m))
+      // ..setForce(
+      //     'center', f.Center(size.width / 2, size.height / 2, strength: 0.5))
+      ..setForce(
+        'edges',
+        f.Edges(edges: graph.edges, distance: 30 * 3.0 * _m),
+      )
+      ..setForce('x', f.XPositioning(x: _size.width / 2))
+      ..setForce('y', f.YPositioning(y: _size.height / 2))
+      ..alpha = 1;
+
+    _graphListenable = _GraphListenable();
+
+    _ticker = createTicker((_) {
+      // print("tick ...");
+      setState(() {
+        simulation.tick();
+      });
+      _graphListenable.notify();
+    })
+      ..start();
 
     transformationController = TransformationController();
   }
 
-  Offset _getLocationPosition(Offset globalPos) {
-    RenderBox graphViewRenderBox = context.findRenderObject() as RenderBox;
+  @override
+  void dispose() {
+    _ticker.dispose();
 
-    var pos = graphViewRenderBox.globalToLocal(globalPos);
-    var matrix = transformationController.value;
-
-    return MatrixUtils.transformPoint(Matrix4.inverted(matrix), pos);
+    super.dispose();
   }
+
+  // Offset _getLocationPosition(Offset globalPos) {
+  //   RenderBox graphViewRenderBox = context.findRenderObject() as RenderBox;
+
+  //   var pos = graphViewRenderBox.globalToLocal(globalPos);
+  //   var matrix = transformationController.value;
+
+  //   return MatrixUtils.transformPoint(Matrix4.inverted(matrix), pos);
+  // }
 
   @override
   Widget build(BuildContext context) {
     var children = <Widget>[];
 
-    children.add(CustomPaint(painter: GraphEdgePainter(widget.graph)));
+    children.add(CustomPaint(
+      painter: GraphEdgePainter(widget.graph, _graphListenable),
+    ));
 
     for (var node in widget.graph.nodes) {
+      if (node.x.isNaN || node.y.isNaN) {
+        continue;
+      }
+
       var w = Positioned(
         child: GestureDetector(
           child: NodeWidget(node, nodeSize),
-          onPanStart: (details) {
-            var pos = _getLocationPosition(details.globalPosition);
-            node.x = pos.dx;
-            node.y = pos.dy;
-            node.pressed = true;
+          // onPanStart: (details) {
+          //   var pos = _getLocationPosition(details.globalPosition);
+          //   node.x = pos.dx;
+          //   node.y = pos.dy;
+          //   // node.pressed = true;
 
-            if (node.y <= nodeSize / 2) {
-              node.y = nodeSize / 2;
-            }
-            if (node.x <= nodeSize / 2) {
-              node.x = nodeSize / 2;
-            }
+          //   if (node.y <= nodeSize / 2) {
+          //     node.y = nodeSize / 2;
+          //   }
+          //   if (node.x <= nodeSize / 2) {
+          //     node.x = nodeSize / 2;
+          //   }
 
-            widget.graph.notify();
-            // print("Pan start ${node.label} $pos");
-          },
-          onPanEnd: (DragEndDetails details) {
-            // print("Pan end ${node.label} $details");
-            node.pressed = false;
-            widget.graph.notify();
-          },
-          onPanUpdate: (details) {
-            var pos = _getLocationPosition(details.globalPosition);
-            node.x = pos.dx;
-            node.y = pos.dy;
+          //   // widget.graph.notify();
+          //   // print("Pan start ${node.label} $pos");
+          // },
+          // onPanEnd: (DragEndDetails details) {
+          //   // print("Pan end ${node.label} $details");
+          //   // node.pressed = false;
+          //   // widget.graph.notify();
+          // },
+          // onPanUpdate: (details) {
+          //   var pos = _getLocationPosition(details.globalPosition);
+          //   node.x = pos.dx;
+          //   node.y = pos.dy;
 
-            if (node.y <= nodeSize / 2) {
-              node.y = nodeSize / 2;
-            }
-            if (node.x <= nodeSize / 2) {
-              node.x = nodeSize / 2;
-            }
+          //   if (node.y <= nodeSize / 2) {
+          //     node.y = nodeSize / 2;
+          //   }
+          //   if (node.x <= nodeSize / 2) {
+          //     node.x = nodeSize / 2;
+          //   }
 
-            widget.graph.notify();
-            // print("Pan update ${node.label} $pos");
-          },
+          //   // widget.graph.notify();
+          //   // print("Pan update ${node.label} $pos");
+          // },
         ),
         left: node.x - (nodeSize / 2),
         top: node.y - (nodeSize / 2),
@@ -148,7 +225,7 @@ class _GraphViewState extends State<GraphView> {
     var view = Container(
       width: 2500,
       height: 2500,
-      color: Colors.white,
+      color: Theme.of(context).scaffoldBackgroundColor,
       child: Stack(
         children: children,
         fit: StackFit.expand,
@@ -166,22 +243,39 @@ class _GraphViewState extends State<GraphView> {
 }
 
 class GraphEdgePainter extends CustomPainter {
+  final _GraphListenable graphListenable;
   final Graph graph;
 
-  GraphEdgePainter(this.graph) : super(repaint: graph);
+  GraphEdgePainter(this.graph, this.graphListenable)
+      : super(repaint: graphListenable);
 
   @override
   void paint(Canvas canvas, Size size) {
     // Draw all the edges
     for (var edge in graph.edges) {
       var strokeWitdth = 2.5;
-      if (edge.a.pressed || edge.b.pressed) {
-        strokeWitdth *= 2;
+
+      // FIXME: Pressed
+      // if (edge.source.pressed || edge.target.pressed) {
+      //   strokeWitdth *= 2;
+      // }
+
+      // print('s ${Offset(edge.source.x, edge.source.y)}');
+      // print('t ${Offset(edge.target.x, edge.target.y)}');
+
+      var source = edge.source;
+      if (source.x.isNaN || source.y.isNaN) {
+        continue;
+      }
+
+      var target = edge.target;
+      if (target.x.isNaN || target.y.isNaN) {
+        continue;
       }
 
       canvas.drawLine(
-        Offset(edge.a.x, edge.a.y),
-        Offset(edge.b.x, edge.b.y),
+        Offset(source.x, source.y),
+        Offset(target.x, target.y),
         Paint()
           ..color = Colors.green
           ..strokeWidth = strokeWitdth,
@@ -196,20 +290,25 @@ class GraphEdgePainter extends CustomPainter {
 }
 
 class NodeWidget extends StatelessWidget {
-  final Node node;
+  final NoteNode node;
   final double size;
 
   const NodeWidget(this.node, this.size);
 
   @override
   Widget build(BuildContext context) {
+    var debug = node.note.filePath == 'docs/eleventy-and-netlify.md';
+    if (debug) {
+      // print("building .. ${node.note.filePath} ${node.x} ${node.y}");
+    }
+
     var theme = Theme.of(context);
     var textStyle = theme.textTheme.subtitle1!.copyWith(fontSize: 8.0);
 
-    var label = node.label!;
-    if (label.startsWith('docs/')) {
-      label = label.substring(5);
-    }
+    var label = node.note.filePath;
+    // if (label.startsWith('docs/')) {
+    //   label = label.substring(5);
+    // }
     return Column(
       children: [
         Container(
