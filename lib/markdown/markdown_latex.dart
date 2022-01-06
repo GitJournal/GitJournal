f2ff4170bd859cb997ab4ffea2a8b2ea81c7b178
Vishesh Handa <me@vhanda.in>
2022-01-06 23:30:27 +0100

    markdown: Implement a KatexInlineParser
    Also try for a KatexBlockParser but that doesn't seem to be working.

--- lib/markdown/markdown_latex.dart
+++ lib/markdown/markdown_latex.dart
@@ -4,34 +4,117 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import 'package:markdown/markdown.dart' as md;
+import 'package:markdown/markdown.dart';
 
-// Mathjax :
-// \\[ x = {-b \pm \sqrt{b^2-4ac} \over 2a} \\]
-// \\( x = {-b \pm \sqrt{b^2-4ac} \over 2a} \\)
-// FIXME: Should it be with a \\ or just \
+// Surrounded by $
+class KatexInlineSyntax extends InlineSyntax {
+  static const String _pattern = r'\$([^$\n]+)\$';
 
-// $$ \begin{equation} \label{label} ... \end{equation} $$
-// `$$x = {-b \pm \sqrt{b^2-4ac} \over 2a}.$$`
-// `$$c=\sqrt{a^2 + b^2}$$`
-// with $$ it can span multiple lines
-
-// For inline mode, you can either use $...$ or \(...\). As an example, \(E = mc^2\) yields E=mc2.
-// Alternatively, \begin{math} and \end{math} can be used.
-
-class MathJaxInlineSyntax extends md.InlineSyntax {
-  static const String _pattern = r'\\(.*\\)';
-
-  MathJaxInlineSyntax() : super(_pattern);
+  KatexInlineSyntax() : super(_pattern);
 
   @override
-  bool onMatch(md.InlineParser parser, Match match) {
-    var text = match[1]!;
+  bool onMatch(InlineParser parser, Match match) {
+    var text = match[1]!.trim();
+    if (text.isEmpty) {
+      return false;
+    }
 
-    var el = md.Element('span', [md.Text(text)]);
-    el.attributes['type'] = 'mathjax';
+    var el = Element.text('span', text);
+    el.attributes['type'] = 'katex';
 
     parser.addNode(el);
     return true;
+  }
+}
+
+// https://pub.dev/packages/flutter_tex_js
+
+final _pattern = RegExp(r'^\$\$([\S\s]*[^$][^$])\$\$$', multiLine: true);
+
+/// Parses Surrounded by $$
+class KatexBlockSyntax extends BlockSyntax {
+  @override
+  RegExp get pattern => _pattern;
+
+  const KatexBlockSyntax();
+
+  @override
+  bool canParse(BlockParser parser) {
+    // print('canParse ${parser.current}');
+    final match = pattern.firstMatch(parser.current);
+    // print('match $match');
+    if (match == null) return false;
+
+    print(match[0]);
+    return false;
+    // final codeFence = match.group(1)!;
+    // final infoString = match.group(2);
+
+    // From the CommonMark spec:
+    //
+    // > If the info string comes after a backtick fence, it may not contain
+    // > any backtick characters.
+    // return (codeFence.codeUnitAt(0) != $backquote ||
+    // !infoString!.codeUnits.contains($backquote));
+  }
+
+  @override
+  List<String> parseChildLines(BlockParser parser, [String? endBlock]) {
+    endBlock ??= '';
+
+    var childLines = <String>[];
+    parser.advance();
+
+    while (!parser.isDone) {
+      var match = pattern.firstMatch(parser.current);
+      if (match == null || !match[1]!.startsWith(endBlock)) {
+        childLines.add(parser.current);
+        parser.advance();
+      } else {
+        parser.advance();
+        break;
+      }
+    }
+
+    return childLines;
+  }
+
+  @override
+  Node parse(BlockParser parser) {
+    // Get the syntax identifier, if there is one.
+    var match = pattern.firstMatch(parser.current)!;
+    var endBlock = match.group(1);
+    var infoString = match.group(2)!;
+
+    var childLines = parseChildLines(parser, endBlock);
+
+    // The Markdown tests expect a trailing newline.
+    childLines.add('');
+
+    var text = childLines.join('\n');
+    if (parser.document.encodeHtml) {
+      // text = escapeHtml(text);
+    }
+    var code = Element.text('code', text);
+
+    // the info-string should be trimmed
+    // http://spec.commonmark.org/0.22/#example-100
+    infoString = infoString.trim();
+    if (infoString.isNotEmpty) {
+      // only use the first word in the syntax
+      // http://spec.commonmark.org/0.22/#example-100
+      var firstSpace = infoString.indexOf(' ');
+      if (firstSpace >= 0) {
+        infoString = infoString.substring(0, firstSpace);
+      }
+      if (parser.document.encodeHtml) {
+        // infoString = escapeHtmlAttribute(infoString);
+      }
+      code.attributes['class'] = 'language-$infoString';
+    }
+
+    var element = Element('pre', [code]);
+
+    return element;
   }
 }
---
+++ test/markdown/markdown_latex_test.dart
@@ -0,0 +1,63 @@
+/*
+ * SPDX-FileCopyrightText: 2019-2021 Vishesh Handa <me@vhanda.in>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import 'package:markdown/markdown.dart' as md;
+import 'package:test/test.dart';
+
+import 'package:gitjournal/markdown/markdown_latex.dart';
+
+void main() {
+  test('Inline', () async {
+    var body = r"""# Hi
+$\\sqrt{3x-1}+(1+x)^2$""";
+
+    var expectedHtml = r"""<h1>Hi</h1>
+<p><span type="katex">\\sqrt{3x-1}+(1+x)^2</span></p>""";
+
+    expect(_convert(body), expectedHtml);
+  });
+
+  test('Inline Empty', () async {
+    var body = r"""# Hi
+$$""";
+
+    var expectedHtml = r"""<h1>Hi</h1>
+<p>$$</p>""";
+
+    expect(_convert(body), expectedHtml);
+  });
+
+  test('Block', () async {
+    var body = r"""# Hi
+$$\begin{array}{c}
+
+\end{array}$$""";
+
+    var expectedHtml = r"""<h1>Hi</h1>
+<p><span type="katex">\begin{array}{c}
+
+\end{array}</span></p>""";
+
+    expect(_convert(body), expectedHtml);
+  }, skip: true);
+}
+
+String _convert(String body) {
+  var lines = body.split('\n');
+
+  var doc = md.Document(
+    encodeHtml: false,
+    extensionSet: md.ExtensionSet(
+      List<md.BlockSyntax>.unmodifiable(<md.BlockSyntax>[
+        const KatexBlockSyntax(),
+      ]),
+      List<md.InlineSyntax>.unmodifiable(<md.InlineSyntax>[]),
+    ),
+    inlineSyntaxes: [KatexInlineSyntax()],
+  );
+  var nodes = doc.parseLines(lines);
+  return md.renderToHtml(nodes);
+}
