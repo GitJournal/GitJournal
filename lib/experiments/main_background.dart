7c4e9de22459244350122b309adecc6cd51795ce
Vishesh Handa <me@vhanda.in>
2022-01-22 14:55:11 +0100

    Enable the background_fetch plugin
    This is just to see if Google accepts a build with the background_fetch
    plugin enabled. Last time, the builds were being automatically rejected.

--- lib/experiments/main_background.dart
+++ lib/experiments/main_background.dart
@@ -4,160 +4,262 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-// import 'dart:async';
+import 'dart:async';
+import 'dart:convert';
 
-// import 'package:flutter/material.dart';
+import 'package:background_fetch/background_fetch.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-// import 'package:background_fetch/background_fetch.dart';
+/// SharedPreferences data key.
+const EVENTS_KEY = "fetch_events";
 
-// [Android-only] This "Headless Task" is run when the Android app
-// is terminated with enableHeadless: true
-// void backgroundFetchHeadlessTask(HeadlessTask task) async {
-//   String taskId = task.taskId;
-//   bool isTimeout = task.timeout;
-//   if (isTimeout) {
-//     // This task has exceeded its allowed running-time.
-//     // You must stop what you're doing and immediately .finish(taskId)
-//     print("[BackgroundFetch] Headless task timed-out: $taskId");
-//     BackgroundFetch.finish(taskId);
-//     return;
-//   }
-//   print('[BackgroundFetch] Headless event received.');
-//   // Do your work here...
-//   BackgroundFetch.finish(taskId);
-// }
+/// This "Headless Task" is run when app is terminated.
+void backgroundFetchHeadlessTask(HeadlessTask task) async {
+  var taskId = task.taskId;
+  var timeout = task.timeout;
+  if (timeout) {
+    print("[BackgroundFetch] Headless task timed-out: $taskId");
+    BackgroundFetch.finish(taskId);
+    return;
+  }
 
-// void main() {
-//   // Enable integration testing with the Flutter Driver extension.
-//   // See https://flutter.io/testing/ for more info.
-//   runApp(MyApp());
+  print("[BackgroundFetch] Headless event received: $taskId");
 
-//   // Register to receive BackgroundFetch events after app is terminated.
-//   // Requires {stopOnTerminate: false, enableHeadless: true}
-//   BackgroundFetch.registerHeadlessTask(backgroundFetchHeadlessTask);
-// }
+  var timestamp = DateTime.now();
 
-// class MyApp extends StatefulWidget {
-//   @override
-//   _MyAppState createState() => _MyAppState();
-// }
+  var prefs = await SharedPreferences.getInstance();
 
-// class _MyAppState extends State<MyApp> {
-//   bool _enabled = true;
-//   int _status = 0;
-//   final List<DateTime> _events = [];
+  // Read fetch_events from SharedPreferences
+  var events = <String>[];
+  var json = prefs.getString(EVENTS_KEY);
+  if (json != null) {
+    events = jsonDecode(json).cast<String>();
+  }
+  // Add new event.
+  events.insert(0, "$taskId@$timestamp [Headless]");
+  // Persist fetch events in SharedPreferences
+  prefs.setString(EVENTS_KEY, jsonEncode(events));
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     initPlatformState();
-//   }
+  if (taskId == 'flutter_background_fetch') {
+    /* DISABLED:  uncomment to fire a scheduleTask in headlessTask.
+    BackgroundFetch.scheduleTask(TaskConfig(
+        taskId: "com.transistorsoft.customtask",
+        delay: 5000,
+        periodic: false,
+        forceAlarmManager: false,
+        stopOnTerminate: false,
+        enableHeadless: true
+    ));
+     */
+  }
+  BackgroundFetch.finish(taskId);
+}
 
-//   // Platform messages are asynchronous, so we initialize in an async method.
-//   Future<void> initPlatformState() async {
-//     // Configure BackgroundFetch.
-//     int status = await BackgroundFetch.configure(
-//         BackgroundFetchConfig(
-//             minimumFetchInterval: 15,
-//             stopOnTerminate: false,
-//             enableHeadless: true,
-//             requiresBatteryNotLow: false,
-//             requiresCharging: false,
-//             requiresStorageNotLow: false,
-//             requiresDeviceIdle: false,
-//             requiredNetworkType: NetworkType.NONE), (String taskId) async {
-//       // <-- Event handler
-//       // This is the fetch-event callback.
-//       print("[BackgroundFetch] Event received $taskId");
-//       setState(() {
-//         _events.insert(0, DateTime.now());
-//       });
-//       // IMPORTANT:  You must signal completion of your task or the OS can punish your app
-//       // for taking too long in the background.
-//       BackgroundFetch.finish(taskId);
-//     }, (String taskId) async {
-//       // <-- Task timeout handler.
-//       // This task has exceeded its allowed running-time.  You must stop what you're doing and immediately .finish(taskId)
-//       print("[BackgroundFetch] TASK TIMEOUT taskId: $taskId");
-//       BackgroundFetch.finish(taskId);
-//     });
-//     print('[BackgroundFetch] configure success: $status');
-//     setState(() {
-//       _status = status;
-//     });
+void main() {
+  // Enable integration testing with the Flutter Driver extension.
+  // See https://flutter.io/testing/ for more info.
+  runApp(MyApp());
 
-//     // If the widget was removed from the tree while the asynchronous platform
-//     // message was in flight, we want to discard the reply rather than calling
-//     // setState to update our non-existent appearance.
-//     if (!mounted) return;
-//   }
+  // Register to receive BackgroundFetch events after app is terminated.
+  // Requires {stopOnTerminate: false, enableHeadless: true}
+  BackgroundFetch.registerHeadlessTask(backgroundFetchHeadlessTask);
+}
 
-//   void _onClickEnable(enabled) {
-//     setState(() {
-//       _enabled = enabled;
-//     });
-//     if (enabled) {
-//       BackgroundFetch.start().then((int status) {
-//         print('[BackgroundFetch] start success: $status');
-//       }).catchError((e) {
-//         print('[BackgroundFetch] start FAILURE: $e');
-//       });
-//     } else {
-//       BackgroundFetch.stop().then((int status) {
-//         print('[BackgroundFetch] stop success: $status');
-//       });
-//     }
-//   }
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-//   void _onClickStatus() async {
-//     int status = await BackgroundFetch.status;
-//     print('[BackgroundFetch] status: $status');
-//     setState(() {
-//       _status = status;
-//     });
-//   }
+class _MyAppState extends State<MyApp> {
+  bool _enabled = true;
+  int _status = 0;
+  List<String> _events = [];
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//             title: const Text('BackgroundFetch Example',
-//                 style: TextStyle(color: Colors.black)),
-//             backgroundColor: Colors.amberAccent,
-//             actions: <Widget>[
-//               Switch(value: _enabled, onChanged: _onClickEnable),
-//             ]),
-//         body: Container(
-//           color: Colors.black,
-//           child: ListView.builder(
-//               itemCount: _events.length,
-//               itemBuilder: (BuildContext context, int index) {
-//                 DateTime timestamp = _events[index];
-//                 return InputDecorator(
-//                     decoration: const InputDecoration(
-//                         contentPadding:
-//                             EdgeInsets.only(left: 10.0, top: 10.0, bottom: 0.0),
-//                         labelStyle: TextStyle(
-//                             color: Colors.amberAccent, fontSize: 20.0),
-//                         labelText: "[background fetch event]"),
-//                     child: Text(timestamp.toString(),
-//                         style: const TextStyle(
-//                             color: Colors.white, fontSize: 16.0)));
-//               }),
-//         ),
-//         bottomNavigationBar: BottomAppBar(
-//             child: Row(children: <Widget>[
-//           ElevatedButton(
-//               onPressed: _onClickStatus, child: const Text('Status')),
-//           Container(
-//               child: Text("$_status"),
-//               margin: const EdgeInsets.only(left: 20.0))
-//         ])),
-//       ),
-//     );
-//   }
-// }
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    // Load persisted fetch events from SharedPreferences
+    var prefs = await SharedPreferences.getInstance();
+    var json = prefs.getString(EVENTS_KEY);
+    if (json != null) {
+      setState(() {
+        _events = jsonDecode(json).cast<String>();
+      });
+    }
+
+    // Configure BackgroundFetch.
+    try {
+      var status = await BackgroundFetch.configure(
+          BackgroundFetchConfig(
+            minimumFetchInterval: 15,
+            /*
+        forceAlarmManager: false,
+        stopOnTerminate: false,
+        startOnBoot: true,
+        enableHeadless: true,
+        requiresBatteryNotLow: false,
+        requiresCharging: false,
+        requiresStorageNotLow: false,
+        requiresDeviceIdle: false,
+        requiredNetworkType: NetworkType.NONE,
+
+         */
+          ),
+          _onBackgroundFetch,
+          _onBackgroundFetchTimeout);
+      print('[BackgroundFetch] configure success: $status');
+      setState(() {
+        _status = status;
+      });
+
+      // Schedule a "one-shot" custom-task in 10000ms.
+      // These are fairly reliable on Android (particularly with forceAlarmManager) but not iOS,
+      // where device must be powered (and delay will be throttled by the OS).
+      BackgroundFetch.scheduleTask(TaskConfig(
+          taskId: "com.transistorsoft.customtask",
+          delay: 10000,
+          periodic: false,
+          forceAlarmManager: true,
+          stopOnTerminate: false,
+          enableHeadless: true));
+    } on Exception catch (e) {
+      print("[BackgroundFetch] configure ERROR: $e");
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+  }
+
+  void _onBackgroundFetch(String taskId) async {
+    var prefs = await SharedPreferences.getInstance();
+    var timestamp = DateTime.now();
+    // This is the fetch-event callback.
+    print("[BackgroundFetch] Event received: $taskId");
+    setState(() {
+      _events.insert(0, "$taskId@${timestamp.toString()}");
+    });
+    // Persist fetch events in SharedPreferences
+    prefs.setString(EVENTS_KEY, jsonEncode(_events));
+
+    if (taskId == "flutter_background_fetch") {
+      // Schedule a one-shot task when fetch event received (for testing).
+      /*
+      BackgroundFetch.scheduleTask(TaskConfig(
+          taskId: "com.transistorsoft.customtask",
+          delay: 5000,
+          periodic: false,
+          forceAlarmManager: true,
+          stopOnTerminate: false,
+          enableHeadless: true,
+          requiresNetworkConnectivity: true,
+          requiresCharging: true
+      ));
+       */
+    }
+    // IMPORTANT:  You must signal completion of your fetch task or the OS can punish your app
+    // for taking too long in the background.
+    BackgroundFetch.finish(taskId);
+  }
+
+  /// This event fires shortly before your task is about to timeout.  You must finish any outstanding work and call BackgroundFetch.finish(taskId).
+  void _onBackgroundFetchTimeout(String taskId) {
+    print("[BackgroundFetch] TIMEOUT: $taskId");
+    BackgroundFetch.finish(taskId);
+  }
+
+  void _onClickEnable(enabled) {
+    setState(() {
+      _enabled = enabled;
+    });
+    if (enabled) {
+      BackgroundFetch.start().then((status) {
+        print('[BackgroundFetch] start success: $status');
+      }).catchError((e) {
+        print('[BackgroundFetch] start FAILURE: $e');
+      });
+    } else {
+      BackgroundFetch.stop().then((status) {
+        print('[BackgroundFetch] stop success: $status');
+      });
+    }
+  }
+
+  void _onClickStatus() async {
+    var status = await BackgroundFetch.status;
+    print('[BackgroundFetch] status: $status');
+    setState(() {
+      _status = status;
+    });
+  }
+
+  void _onClickClear() async {
+    var prefs = await SharedPreferences.getInstance();
+    prefs.remove(EVENTS_KEY);
+    setState(() {
+      _events = [];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const EMPTY_TEXT = Center(
+        child: Text(
+            'Waiting for fetch events.  Simulate one.\n [Android] \$ ./scripts/simulate-fetch\n [iOS] XCode->Debug->Simulate Background Fetch'));
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+            title: const Text('BackgroundFetch Example',
+                style: TextStyle(color: Colors.black)),
+            backgroundColor: Colors.amberAccent,
+            foregroundColor: Colors.black,
+            actions: <Widget>[
+              Switch(value: _enabled, onChanged: _onClickEnable),
+            ]),
+        body: (_events.isEmpty)
+            ? EMPTY_TEXT
+            : Container(
+                child: ListView.builder(
+                    itemCount: _events.length,
+                    itemBuilder: (context, index) {
+                      var event = _events[index].split("@");
+                      return InputDecorator(
+                          decoration: InputDecoration(
+                              contentPadding: EdgeInsets.only(
+                                  left: 5.0, top: 5.0, bottom: 5.0),
+                              labelStyle:
+                                  TextStyle(color: Colors.blue, fontSize: 20.0),
+                              labelText: "[${event[0].toString()}]"),
+                          child: Text(event[1],
+                              style: TextStyle(
+                                  color: Colors.black, fontSize: 16.0)));
+                    }),
+              ),
+        bottomNavigationBar: BottomAppBar(
+            child: Container(
+                padding: EdgeInsets.only(left: 5.0, right: 5.0),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      ElevatedButton(
+                          onPressed: _onClickStatus,
+                          child: Text('Status: $_status')),
+                      ElevatedButton(
+                          onPressed: _onClickClear, child: Text('Clear'))
+                    ]))),
+      ),
+    );
+  }
+}
+
 
 // Stuff to be saved -
 // (gitRepo, remoteName, lastSyncedWhen, lastError)
