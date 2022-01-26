ed76e91a611c06daea735c3b51e6d05dbde784bf
Vishesh Handa <me@vhanda.in>
2022-01-26 11:58:29 +0100

    Add missing reuse header

--- test/lib.dart
+++ test/lib.dart
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Vishesh Handa <me@vhanda.in>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 import 'package:dart_git/plumbing/git_hash.dart';
 import 'package:process_run/process_run.dart';
 
