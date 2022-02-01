203cc7be5743b54e6ff4b0ee7046265ae217b707
Vishesh Handa <me@vhanda.in>
2022-02-02 02:05:28 +0400

    Remove useless code

--- lib/core/checklist.dart
+++ lib/core/checklist.dart
@@ -55,12 +55,6 @@ class Checklist {
     multiLine: false,
   );
 
-  // FIXME: Added on 2020-05-01: Remove after 3-4 months
-  static final _oldPattern = RegExp(
-    r'^ *\[([ xX])\] +(.*)$',
-    multiLine: false,
-  );
-
   final Note _note;
   List<ChecklistItem> items = [];
 
@@ -73,10 +67,6 @@ class Checklist {
 
     for (var i = 0; i < _lines.length; i++) {
       var line = _lines[i];
-      var oldPatMatch = _oldPattern.firstMatch(line);
-      if (oldPatMatch != null) {
-        line = '- ' + line;
-      }
 
       var match = _pattern.firstMatch(line);
       if (match == null) {
