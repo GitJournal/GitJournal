d3d63ff4265a8bcd978411a84937009edb919c18
Vishesh Handa <me@vhanda.in>
2022-02-28 14:06:01 +0100

    Improve rename test

--- test/editors/note_editor_test.dart
+++ test/editors/note_editor_test.dart
@@ -139,5 +139,9 @@ void main() {
     expect(repo.rootFolder.getNoteWithSpec('doc.md'), null);
     expect(repo.rootFolder.getNoteWithSpec('bugs.md'), isNotNull);
     expect(repo.rootFolder.getAllNotes().length, 5);
+
+    // Check the view
+    saveButtonFinder = find.byKey(const ValueKey('NewEntry'));
+    expect(saveButtonFinder, findsNothing);
   });
 }
