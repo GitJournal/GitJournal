f6eac1071765e276dbd5a0030346252dd459ecd6
Vishesh Handa <me@vhanda.in>
2022-06-25 16:04:42 +0200

    Send the Analytics data as an http post
    Using DIO for sending binary data is very very difficult. It has taken
    me over 45 minutes to figure out that Dio changes the body in some
    strange ass manner unless I sent it as a stream.
    
    WTF is this strange library.
    
    Related -
    * https://github.com/flutterchina/dio/issues/307
    * https://github.com/flutterchina/dio/issues/837

--- lib/analytics/network.dart
+++ lib/analytics/network.dart
@@ -4,27 +4,48 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import 'package:grpc/grpc.dart';
-
 import 'package:gitjournal/.env.dart';
-import 'package:gitjournal/analytics/generated/analytics.pbgrpc.dart';
+import 'package:gitjournal/logger/logger.dart';
 import 'package:gitjournal/utils/result.dart';
 import 'generated/analytics.pb.dart' as pb;
 
 import 'package:dio/dio.dart';
+import 'package:dio_smart_retry/dio_smart_retry.dart';
 
-const _port = 443;
-const _timeout = Duration(seconds: 120);
+late final dio = () {
+  var d = Dio();
+  d.options.connectTimeout = 10000; // 10 sec
+  d.options.receiveTimeout = 10000;
 
-var dio = Dio();
+  d.interceptors.add(RetryInterceptor(
+    dio: d,
+    logPrint: (x) => Log.d(x),
+    retries: 3,
+    retryDelays: const [
+      Duration(seconds: 1),
+      Duration(seconds: 5),
+      Duration(seconds: 15),
+    ],
+  ));
+  // d.interceptors.add(LogInterceptor(responseBody: true));
+  return d;
+}();
 
 Future<Result<void>> sendAnalytics(pb.AnalyticsMessage msg) async {
+  assert(Env.analyticsUrl.isNotEmpty);
+
   try {
+    final data = msg.writeToBuffer();
     await dio.post(
       Env.analyticsUrl,
-      data: msg.writeToJson(),
+      // vHanda: Send POST data in DIO is so strange. It seems to mess up the data
+      //         if I just pass the Uint8List
+      data: Stream.fromIterable(data.map((e) => [e])),
       options: Options(
-        headers: {Headers.contentTypeHeader: "application/json"},
+        contentType: "application/x-protobuf",
+        headers: {
+          Headers.contentLengthHeader: data.lengthInBytes.toString(),
+        },
       ),
     );
   } catch (ex, st) {
