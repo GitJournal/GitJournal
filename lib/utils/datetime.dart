9a14048bf67c1cbb2eb20d55eb171fc63f587cf3
Vishesh Handa <me@vhanda.in>
2022-08-25 11:21:31 +0200

    License change some files
    Any files with external contributions need to be Apache2 licensed, since
    I don't have any CLA, and need to release this on the ios store where
    *GPL apps are a problem.

--- lib/utils/datetime.dart
+++ lib/utils/datetime.dart
@@ -1,7 +1,7 @@
 /*
  * SPDX-FileCopyrightText: 2019-2021 Vishesh Handa <me@vhanda.in>
  *
- * SPDX-License-Identifier: AGPL-3.0-or-later
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 import 'dart:core';
