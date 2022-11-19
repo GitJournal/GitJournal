272b62f2b08f9b87de6f0b57d90ff94453ff1110
Vishesh Handa <me@vhanda.in>
2022-11-19 03:53:42 +0100

    Fix reuse licensing header

--- lib/app_localizations_context.dart
+++ lib/app_localizations_context.dart
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Vishesh Handa <me@vhanda.in>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 import 'package:flutter/widgets.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
