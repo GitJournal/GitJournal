fb9f54595e47e2f92844cf4442fada411e93f169
Vishesh Handa <me@vhanda.in>
2022-11-27 20:49:38 +0100

    Remove yaml based translations

--- scripts/upload_translation_en.sh
+++ scripts/upload_translation_en.sh
@@ -11,15 +11,11 @@ cd "$(dirname "$0")"
 ID=$(awk '{print $1}' secrets/poeditor-api-key.txt)
 TOKEN=$(awk '{print $2}' secrets/poeditor-api-key.txt)
 
-# Must have a .yml extension
-FILE_PATH="../assets/langs/en.yaml"
-cp "$FILE_PATH" /tmp/en.yml
-
 curl -X POST https://api.poeditor.com/v2/projects/upload \
     -F api_token="$TOKEN" \
     -F id="$ID" \
     -F updating="terms_translations" \
-    -F file=@"/tmp/en.yml" \
+    -F file=@"../lib/l10n/app_en.arb" \
     -F language=en \
     -F overwrite=1 \
     -F fuzzy_trigger=1 \
