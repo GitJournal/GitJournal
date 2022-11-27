cbb40212b0c8088471fc813f66312c31e2408c26
Vishesh Handa <me@vhanda.in>
2022-11-27 20:57:40 +0100

    Get all translations as arb files
    I'm not convinced that committing all of these in Git is the best idea.
    Let see.

--- scripts/download_translations.sh
+++ scripts/download_translations.sh
@@ -13,12 +13,12 @@ TOKEN=$(awk '{print $2}' secrets/poeditor-api-key.txt)
 
 langs=$(curl -s -X POST https://api.poeditor.com/v2/languages/list -d api_token="$TOKEN" -d id="$ID" | jq -r .result.languages[].code)
 
-cd "../assets/langs"
+cd "../lib/l10n"
 for lang in $langs; do
     echo "Downloading for $lang"
 
-    url=$(curl -s -X POST https://api.poeditor.com/v2/projects/export -d api_token="$TOKEN" -d id="$ID" -d language="$lang" -d type="yml" | jq -r .result.url)
-    wget -q -O "$lang.yaml" "$url"
+    url=$(curl -s -X POST https://api.poeditor.com/v2/projects/export -d api_token="$TOKEN" -d id="$ID" -d language="$lang" -d type="arb" | jq -r .result.url)
+    wget -q -O "app_$lang.arb" "$url"
 done
 
 echo "Done"
