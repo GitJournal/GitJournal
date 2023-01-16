d2550d1c8ea637b1e149bc676dd2521f7d0283c2
Vishesh Handa <me@vhanda.in>
2023-01-16 23:10:50 +0100

    Android Build: Remove verbosity
    Not required. It creates too much noise.

--- scripts/build_android.sh
+++ scripts/build_android.sh
@@ -9,13 +9,13 @@ set -eux pipefail
 BUILD_NUM=$(git rev-list --count HEAD)
 echo "Build Number: $BUILD_NUM"
 
-flutter build appbundle --flavor prod --build-number="$BUILD_NUM" --dart-define=INSTALL_SOURCE=playstore --verbose
+flutter build appbundle --flavor prod --build-number="$BUILD_NUM" --dart-define=INSTALL_SOURCE=playstore
 
 BUILD_ID=$(make version | tail -n 1 | awk '{ print $4 }')
 
 # Also building the apk, as it's useful for non Google Play users
-flutter build apk --flavor prod --build-number="$BUILD_NUM" --dart-define=INSTALL_SOURCE=fdroid --verbose
+flutter build apk --flavor prod --build-number="$BUILD_NUM" --dart-define=INSTALL_SOURCE=fdroid
 cp build/app/outputs/flutter-apk/app-prod-release.apk io.gitjournal.gitjournal_$BUILD_ID.apk
 
-flutter build apk --flavor dev --build-number="$BUILD_NUM" --dart-define=INSTALL_SOURCE=fdroid --verbose
+flutter build apk --flavor dev --build-number="$BUILD_NUM" --dart-define=INSTALL_SOURCE=fdroid
 cp build/app/outputs/flutter-apk/app-dev-release.apk io.gitjournal.gitjournal.dev_$BUILD_ID.apk
